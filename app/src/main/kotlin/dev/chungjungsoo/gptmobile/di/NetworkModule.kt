42628bbfd3dfed9e5589e974c755643ef7ab5c69
LanXin <lanxin@astrbot.app>
2026-07-12 03:32:54 +0000

    fix build #12: add missing LanXinAuthClient import in NetworkModule.kt

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/NetworkModule.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/di/NetworkModule.kt
@@ -12,6 +12,7 @@ import com.lanxin.android.data.network.GroqAPI
 import com.lanxin.android.data.network.GroqAPIImpl
 import com.lanxin.android.data.network.LanXinAPI
 import com.lanxin.android.data.network.LanXinAPIImpl
+import com.lanxin.android.data.network.LanXinAuthClient
 import com.lanxin.android.data.network.NetworkClient
 import com.lanxin.android.data.network.OpenAIAPI
 import com.lanxin.android.data.network.OpenAIAPIImpl
