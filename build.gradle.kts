a1e9595a36e832b24d19405e7175e3cb411a36ea
Cuering <107449740+Cuering@users.noreply.github.com>
2026-07-14 21:01:42 +0800

    fix: root 启用 kotlin-kapt 插件（ObjectBox 代码生成需要）

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.auto.license) apply false
     alias(libs.plugins.objectbox) apply false
